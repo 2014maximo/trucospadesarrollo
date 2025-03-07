5e32c2dac0008b29e26ec88a5c7c5e07a20f7220
auriel1 <auriel@smartsoftlabs.com>
2025-03-07 08:37:47 -0500

    Configuration Firebase Hosting with new repository

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import {provideHttpClient} from "@angular/common/http";
 import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
 
 import { routes } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, 'assets/i18n/', '.json');
@@ -21,6 +23,6 @@ export const appConfig: ApplicationConfig = {
         useFactory: httpLoaderFactory,
         deps: [HttpClient],
       },
-    })])
+    })]), provideFirebaseApp(() => initializeApp({ projectId: "blog-4e57a", appId: "1:520935042224:web:3cc6b6edf337477f4a1f75", databaseURL: "https://blog-4e57a-default-rtdb.firebaseio.com", storageBucket: "blog-4e57a.appspot.com", apiKey: "AIzaSyB4kw9EnRuygD-Jeku34jgrDJkmhmRzyY4", authDomain: "blog-4e57a.firebaseapp.com", messagingSenderId: "520935042224", measurementId: "G-QZ97PNTT9E" })), provideFirestore(() => getFirestore())
   ]
 };
