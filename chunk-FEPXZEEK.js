900f3269d01c8e943426d542fe07c22e600412fd
auriel1 <auriel@smartsoftlabs.com>
2025-08-15 08:41:00 -0500

    Auto-generated commit

---
+++ chunk-FEPXZEEK.js
@@ -0,0 +1,1 @@
+import"./chunk-2VMXMS7J.js";var t=[{path:"angular",loadChildren:()=>import("./chunk-4JRUFPZJ.js").then(o=>o.ANGULAR_ROUTES)},{path:"developer",loadChildren:()=>import("./chunk-TSABPA6A.js").then(o=>o.DEVELOPER_ROUTES)}];export{t as BLOG_ROUTES};
