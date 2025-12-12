2cc826065df20c7b55f11895953788d010e2cb80
auriel1 <auriel@smartsoftlabs.com>
2025-12-11 21:53:24 -0500

    add initial content category angular

--- src/app/features/blog/posts/angular/angular-category/angular-category.component.ts
+++ src/app/features/blog/posts/angular/angular-category/angular-category.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CategoriaPostModel } from '@models/categorias.model';
+import { CategoryHeaderComponent } from 'src/app/shared/components/category-header/category-header.component';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+import { GoUpComponent } from 'src/app/shared/components/go-up/go-up.component';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { AddsModel } from 'src/app/shared/models/post.model';
 
 @Component({
   selector: 'app-angular-category',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CategoryHeaderComponent,
+    GoUpComponent],
   templateUrl: './angular-category.component.html',
   styleUrl: './angular-category.component.css'
 })
 export class AngularCategoryComponent {
+
+  public idPost = 'eb4011f5-81df-4261-b5c5-8f58d2ba88b5';
+  public partsAdds: AddsModel = { contentIndex: true, roadMap: true, origin: true };
+  public categoria = new CategoriaPostModel();
 
   constructor(){
   }
