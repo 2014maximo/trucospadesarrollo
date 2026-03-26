47669e3c4e43aa3318f49a77e1803bab913c23f4
2014maximo <alexuriel.777@gmail.com>
2026-03-25 22:19:29 -0500

    add content post liks

--- src/app/features/blog/posts/developer/dev-sites/constants/index-buttons.constant.ts
+++ src/app/features/blog/posts/developer/dev-sites/constants/index-buttons.constant.ts
@@ -54,6 +54,11 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                 title: ' ', ImgSlider: {
                     alt: 'Envato Logo', src: 'assets/img/icons/envato.png', height: 40, width: 0
                 }, description: 'Miles de plantillas en multiples formatos, cuando se ponen en venta la creatividad no tiene limites.', url: 'https://elements.envato.com/es/graphic-templates/websites'
+            },
+            {
+                title: 'PAPERME', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Plantillas de papel con muchos diseños exportables a PDF.', url: 'https://paperme.pixzens.com/es'
             }
         ]
     },
@@ -163,6 +168,16 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                 }, description: 'Productos digitales muy asequibles. Juegos, Suscripciones, Códigos promocionales.', url: 'https://nasusrp.store/'
             },
             {
+                title: 'Recoded:88', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Juegos online de las consolas retro, evita instalar emuladores solo juega y listo. Nes, Snes, Sega Genesis, Nintendo 64, NeoGeo, Playstation, Game boy, Game boy advance y Nintendo DS', url: 'https://rec0ded88.com/'
+            },
+            {
+                title: 'Repositorio con código fuente de juegos reconocidos', ImgSlider: {
+                    alt: '', src: 'cons/xbox.png', height: 0, width: 0
+                }, description: 'Repositorio Github con versiones de juegos como Doom, Mario 64, para jugar o analizar como esta construída su lógica.', url: 'https://github.com/bobeff/open-source-games'
+            },
+            {
                 title: ' ', ImgSlider: {
                     alt: 'Xbox Logo', src: 'assets/img/icons/xbox.png', height: 40, width: 0
                 }, description: 'Xbox Official Site, tan simple como lo grande que es.', url: 'https://www.xbox.com/'
@@ -214,6 +229,16 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                 title: 'Sobrief', ImgSlider: {
                     alt: '', src: '', height: 0, width: 0
                 }, description: 'Resúmenes de libros.', url: 'https://sobrief.com/'
+            },
+            {
+                title: 'Welib', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Biblioteca digital de libros. ', url: 'https://welib-public.org/es.html'
+            },
+            {
+                title: 'Open Library', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Al viejo estilo de prestar libros en la biblioteca para devolverlos después. Creas tu cuenta y listo ya tienes acceso al universo de conocimientos. ', url: 'https://openlibrary.org/'
             }
         ]
     },
@@ -531,7 +556,70 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                     alt: '', src: '', height: 0, width: 0
                 }, description: 'Herramienta IA crear modelos 3D con excelentes maneras de aplicación.', url: 'https://www.meshy.ai/'
             },
+            {
+                title: 'Pixverse', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Edición gráfica con IA. Fotos y videos, desde una videos ya capturado hacer los cambios que se le pidan. Versiones de pago.', url: 'https://app.pixverse.ai/home'
+            },
+            {
+                title: 'MagicHour', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Edición gráfica con IA. Fotos y videos, desde una videos ya capturado hacer los cambios que se le pidan. Versiones de pago.', url: 'https://magichour.ai/'
+            },
+            {
+                title: 'ObjectRemover', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'IA para eliminar objetos de imágenes, algo simple en descripción pero una utilidad muy requerida.', url: 'https://www.objectremover.com/'
+            },
+            {
+                title: 'Zoo Dev', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'IA para crear objetos 3d a partir de una descripción en texto, los modelos se pueden descargar para utilizar.', url: 'https://zoo.dev/signin?callbackUrl=https%3A%2F%2Fapp.zoo.dev'
+            },
 
+        ]
+    },
+    {
+        id: 'iatextvox',
+        name: 'IA - TEXTO A VOZ',
+        individualStyle: {
+            backgroundColor: 'bg-Dart',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-2'
+        },
+        resources: [
+            {
+                title: 'Speechma', ImgSlider: {
+                    alt: '', src: '', height: 60, width: 0
+                }, description: 'IA para conversión de texto a voz, te llenan la web de comerciales y propagandas pero es funcional y tiene muchas voces en muchos idiomas.', url: 'https://speechma.com/english'
+            },
+            {
+                title: 'Lovevoice', ImgSlider: {
+                    alt: '', src: '', height: 60, width: 0
+                }, description: 'IA para conversión de texto a voz, una vez te logueas te aumentan la cantidad de carácteres a 20.000 y tiene muchas voces en muchos idiomas.', url: 'https://lovevoice.ai/'
+            },
+            {
+                title: 'Qwen-3', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Qwen-3 es un modelo de lenguaje grande desarrollado por Alibaba Cloud, especializado en tareas de comprensión y generación de texto y voz.', url: 'https://qwen-3.com/en'
+            },
+            {
+                title: 'LuvVoice', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Texto a voz online a través de IA con versiones de pago.', url: 'https://luvvoice.com/'
+            },
+            {
+                title: 'Minimax', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Texto a voz online a través de IA con versiones de pago.', url: 'https://www.minimax.io/audio/text-to-speech'
+            },
+            {
+                title: 'NaturalReaders', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Texto a voz online a través de IA avatares demasiado virtualizadas, la interfaz esta muy bien desarrolladas y cumple al final. Como todas tiene versiones de pago, hay que patrocinar el negocio.', url: 'https://www.naturalreaders.com/online/'
+            }
         ]
     },
     {
@@ -555,6 +643,11 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                     alt: '', src: '', height: 0, width: 0
                 }, description: 'Es una herramienta IA, como en especie de grupo de empleados haciendo muchas tareas, adicional se puede integrar con otras aplicaciones para gestionar como el google calendar hacer una investigación y agregar eventos y tareas en el calendario.', url: 'https://www.kortix.com/'
             },
+            {
+                title: 'ChatLevel', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Automatización de chat de tus redes sociales como whatsapp, facebook, instagram. Integra un promp con la info que quieres que la IA tenga en cuenta para las respuestas.', url: 'https://chatlevel.ai/'
+            }
         ]
     },
         {
@@ -591,7 +684,12 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                 title: 'Toffeeshare', ImgSlider: {
                     alt: '', src: 'https://toffeeshare.com/static/media/logo.42a58a0d.png', height: 60, width: 0
                 }, description: 'Una forma de enviar archivos sin limite de tamaño, sin que se queden con tus archivos, de modo peer to peer.', url: 'https://toffeeshare.com/es'
-            }
+            },
+            {
+                title: 'Fromsmash', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Una forma de enviar archivos sin pagar ni registrarse.', url: 'https://fromsmash.com/es'
+            },
         ]
     },
     {
@@ -646,6 +744,121 @@ export const INDEX_BUTTONS_DEV_SITES: IndexButtonsModel[] = [
                     alt: '', src: '', height: 0, width: 0
                 }, description: 'Aplicación para crear y seguir planes de ejercicio.', url: 'https://workout.cool/es'
             }
+        ]
+    },
+    {
+        id: 'tecnologia',
+        name: 'Tecnología',
+        individualStyle: {
+            backgroundColor: 'bg-Linux',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-1'
+        },
+        resources: [
+            {
+                title: 'Diode', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Plataforma de simulación de interfaces en electrónica.', url: 'https://www.withdiode.com/'
+            },
+            {
+                title: 'Wokwi', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Escenarios de electrónica que la virtualización ha traido a la mano para trabajar como no se ubiese imagina hace algún tiempo.', url: 'https://wokwi.com/'
+            }
+        ]
+    },
+    {
+        id: 'aprendizaje',
+        name: 'Aprendizaje',
+        individualStyle: {
+            backgroundColor: 'bg-Ionic',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-1'
+        },
+        resources: [
+            {
+                title: 'ClassCentral', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Simplemente brutal. En lo que tiene que ver con conseguir las fuentes de gran demanda en cursos y conocimientos, lo recomiendo.', url: 'https://www.classcentral.com/'
+            },
+            {
+                title: 'Claude Code in Action', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Curso de Anthropic con toda la información de las diferentes formas de usar Claude para escribir y refactorizar código.', url: 'https://anthropic.skilljar.com/claude-code-in-action'
+            },
+            {
+                title: 'Mindluster', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Aprende lo que necesites con cursos y muchos otros contenidos.', url: 'https://www.mindluster.com/'
+            },
+            {
+                title: 'Labpsi', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Ejercicios Cognitivos y de memoria.', url: 'https://labpsi.mdp.edu.ar/Home/TipoTest/4?descripcion=Ejercicios%20Cognitivos'
+            },
+            {
+                title: 'StudyFlash', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Ayuda de estudio en los exámenes.', url: 'https://studyflash.ai/es'
+            },
+        ]
+    },
+    {
+        id: 'mapas',
+        name: 'Mapas',
+        individualStyle: {
+            backgroundColor: 'bg-Javascript',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-1'
+        },
+        resources: [
+            {
+                title: 'TopoExport', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Selecciona un área y descarga sus datos en varios formatos y vistas 2d y 3d, versiones de pago.', url: 'https://app.topoexport.com/'
+            },
+        ]
+    },
+    {
+        id: 'interesante',
+        name: 'Interesante',
+        individualStyle: {
+            backgroundColor: 'bg-Javascript',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-1'
+        },
+        resources: [
+            {
+                title: 'Tastedive', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Página web de las recomendaciones, o relacionados o similares. Solo es buscar un libro, una película o un juegos, por nombre o por artista, te entrega algo muy útil en una lista de recomendados. Bastante práctico cuando eres amante de un género específico.', url: 'https://tastedive.com/'
+            },
+        ]
+    },
+    {
+        id: 'gastronomia',
+        name: 'Gastronomía',
+        individualStyle: {
+            backgroundColor: 'bg-Javascript',
+            textColor: 'text-white',
+            fontFamily: 'fuenteSeis',
+            fontSize: 'fs-30',
+            rotation: 'rot-1'
+        },
+        resources: [
+            {
+                title: 'Supercook', ImgSlider: {
+                    alt: '', src: '', height: 0, width: 0
+                }, description: 'Si tienes algunos ingredientes, Supercook te ayuda a crear recetas. .', url: 'https://www.supercook.com/#/desktop'
+            },
         ]
     },
 
