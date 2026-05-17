8ce50c18b601d29192f04ac5c4ef976127135f22
2014maximo <alexuriel.777@gmail.com>
2026-05-17 13:07:50 -0500

    checkout changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  graphql: 'https://api.trucospadesarrollo.com/graphql'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  graphql: 'http://localhost/wordpress/graphql'
+};
